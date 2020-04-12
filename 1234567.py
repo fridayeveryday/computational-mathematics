2e0c49990b14bcdba28adf4d7f26a11b77dc4cf5
Pavel <50109083+fridayeveryday@users.noreply.github.com>
2020-04-13 00:35:11 +0400

    work version

--- 1234567.py
+++ 1234567.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def outprint_matrix(matrix):
     for i in range(matrix.len):
         for j in range(matrix.len):
@@ -15,7 +16,7 @@ def input_free_var_column(size):
     col = []
     for i in range(size):
         col.append(int(input()))
-    return col
+    return np.array([col])
 
 
 def check_convergence(matrix):
@@ -26,33 +27,89 @@ def check_convergence(matrix):
             return False
     return True
 
+
+def check_inequality(matrix_B, current_vector_X, prev_vector_X):
+    difference = np.subtract(current_vector_X, prev_vector_X)
+    difference = max(abs(difference))
+    difference = difference[0]
+    norm_B = np.linalg.norm(matrix_B, ord=np.inf)
+    right_part = (1 - norm_B)
+    right_part = right_part / np.linalg.norm(matrix_B, ord=np.inf)
+    right_part = right_part * epsilon
+
+    # if np.subtract(current_vector_X, prev_vector_X) <= (1-np.linalg.norm(matrix_B, ord=np.inf))/np.linalg.norm(matrix_B, ord=np.inf) * epsilon:
+    if difference <= right_part:
+        return False
+    else:
+        return True
+
+
 def iterating(matrix_B, vector_of_free, current_vector_X, prev_vector_X):
-    while True:
+    while check_inequality(matrix_B, current_vector_X, prev_vector_X):
+        prev_vector_X = current_vector_X
+        current_vector_X = np.dot(matrix_B, prev_vector_X)
+
+        current_vector_X = current_vector_X + vector_of_free
+
+    return current_vector_X
 
 
-size = int(input())
-matrix = input_matrix(size)
-matrix = np.array(matrix)
+def is_convergence(matrix):
+    for i in range(np.size(matrix, 0)):
+        if sum(matrix[i]) - matrix[i][i] >= matrix[i][i]:
+            print("Jacobi method don't convergence. Please enter another matrix")
+            return False
+    print("Jacobi method convergence")
+    return True
 
-print(np.linalg.norm(matrix, ord=np.inf))
-free_var_columns = np.array(input_free_var_column(size))
-print(free_var_columns, free_var_columns.shape,  end='\n\n')
-print(np.transpose(free_var_columns), np.transpose(free_var_columns).shape,  end='\n\n')
-print(np.dot(matrix, free_var_columns), end='\n\n')
-print(np.dot(matrix, np.transpose(free_var_columns)), end='\n')
-print(matrix.shape)
-free_var_columns = free_var_columns.reshape(2, 1)
+def out_res_vector(res_vector):
+    np.ndarray.tolist(res_vector)
+    for i in range(np.size(res_vector)):
+        print('x{} = {}'.format(i, res_vector[i]))
 
+matrix = None
+size = 0
+
+
+def input_size_and_matrix():
+    print("Enter the size of The matrix")
+    global size
+    size = int(input())
+    print("Enter The matrix")
+    global matrix
+    matrix = input_matrix(size)
+    matrix = np.array(matrix)
+
+
+input_size_and_matrix()
+method_is_convergencer = is_convergence(matrix)
+if not method_is_convergencer:
+    while not method_is_convergencer:
+        input_size_and_matrix()
+
+epsilon = 0.0001  # accuracy
+
+print("Enter The free var columns")
+free_var_columns = input_free_var_column(size)
 
 matrix_D = np.diag(np.diag(matrix))
 matrix_L = np.tril(matrix, -1)
-matrix_R = np.tril(matrix, 1)
-matrix_B = (-1) * np.dot(np.linalg.inv(matrix_D) * (matrix_L + matrix_R))
-vector_c = np.dot(np.linalg.inv(matrix_D), free_var_columns)
+matrix_R = np.triu(matrix, 1)
 
-norm_of_vector_c = max(vector_c)
+inv_mat_D = np.linalg.inv(matrix_D)
+sum = matrix_L + matrix_R
+matrix_B = np.dot(np.linalg.inv(matrix_D), (matrix_L + matrix_R))
+matrix_B = np.dot(matrix_B, -1)
+free_var_columns = np.transpose(free_var_columns)
+vector_c = np.dot(inv_mat_D, free_var_columns)
+
+norm_of_vector_c = max(abs(vector_c))
 norm_of_matrix_B = np.linalg.norm(matrix_B, ord=np.inf)
 
-vector_X = [0]*size
+vector_X = [0] * size
+prev_vec = np.array([[0], [0]])
 
-
+vector_X = np.dot(matrix_B, prev_vec) + vector_c
+# res = \
+res = iterating(matrix_B, vector_c, vector_X, prev_vec)
+out_res_vector(res)
