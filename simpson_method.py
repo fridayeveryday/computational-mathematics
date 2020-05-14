b49743f7a1bba52de1f1d150843f41f4da695c87
Pavel <50109083+fridayeveryday@users.noreply.github.com>
2020-05-14 22:15:59 +0400

    Improved a output format of an answer

--- simpson_method.py
+++ simpson_method.py
@@ -1,11 +1,14 @@
-# increment of the integration step
-w = 1
+import math
 
-# range of the integration
+# an increment of the integration step
+w = 1
+# a degree of the method
+p = 4
+# a range of the integration
 a = 1
 b = 2
 
-# step
+# a step
 h = 0.0625 * w
 # a number of steps
 n = 16
@@ -29,12 +32,28 @@ def integrate():
 
         x += h
         even = not even
-    sum += (expression(a)+expression(b))/2
-    return (2*h/3) * sum
+    sum += (expression(a) + expression(b)) / 2
+    return (2 * h / 3) * sum
 
 
 def main():
-    print("result is", integrate())
+    # a result with a unchanged step
+    res1 = integrate()
+    print("a result with the unchanged step is ", res1)
+    print(
+        "************************************************************************************************************")
+    global h, p
+    h = h * 2
+    res2 = integrate()
+    print("a result with the unchanged step is ", res1)
+    print("a result with the doubled step is   ", res2)
+    inaccuracy = (res1 - res2) / (2 ** p - 1)
+    # get a degree of the first non zero digital
+    degree = abs(int(math.log10(abs(inaccuracy))))
+    # make a format string to output
+    format_str = "%." + str(degree + 1) + "f"
+    print("a result is {} ± {}".format(round(res1, degree + 1), format_str % (inaccuracy)))
+
 
 if __name__ == '__main__':
     main()
